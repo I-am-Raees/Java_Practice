60e9db175043ffe2ea2f7229b1a799f5f4b2080f
RAEES AHMAD <88240934+I-am-Raees@users.noreply.github.com>
2022-04-10 10:25:57 +0500

    Here is the first for OO

---
+++ OOP_FIRST.java
@@ -0,0 +1,136 @@
+package com.company;
+
+import javax.naming.Name;
+import java.util.Scanner;
+import java.util.SortedMap;
+
+
+//First class
+// class Employee_1{
+//
+//    int Salary;
+//    String Name;
+//
+//    public int getSalary(){
+//        return Salary;
+//    }
+//
+//    public String getName(){
+//        return Name;
+//    }
+//
+//    public void  setName(String n){
+//        Name=n;
+//    }
+//}
+
+
+//Second class
+// class CellPhone_1{
+//    public void Vibrate(){
+//        System.out.println("Vibrating...");
+//    }
+//    public void Ring(){
+//        System.out.println("Ringing...");
+//    }
+//}
+
+
+
+// Third Class
+//class Area_Perimeter{
+//    int side;
+//    public int getArea(){
+//        int ar=side*side;
+//        return ar;
+//    }
+//    public int getPerimeter(){
+//        int per=side*4;
+//        return per;
+//    }
+//}
+
+//Fourth class
+// class Tommy_vecetti{
+
+//    public void Run(){
+//        System.out.println(" Running ");
+//    }
+//    public void Hit(){
+//        System.out.println(" Hitting ");
+//    }
+//    public void Fire(){
+//        System.out.println(" Firing ");
+//    }
+//
+//}
+
+
+
+
+public class OOP_FIRST {
+
+
+    public static void main(String[] args) {
+
+        //Problem 1
+//    Employee_1 obj_1= new Employee_1();
+//
+//    obj_1.getSalary();
+//    obj_1.setName("Raees");
+//        System.out.println(obj_1.getName());
+//        System.out.println(obj_1.getSalary());
+
+// Problem 2
+//CellPhone_1 ab= new CellPhone_1();
+//    ab.Ring();
+//    ab.Vibrate();
+
+// Problem 3
+//        Area_Perimeter abc=new Area_Perimeter();
+//        Scanner in=new Scanner(System.in);
+//        System.out.print("Enter the value of side : ");
+//        int x=in.nextInt();
+//        abc.side=x;
+//        System.out.println(abc.getArea());
+//        System.out.println(abc.getPerimeter());
+//    }
+
+
+//        Tommy_vecetti ok=new Tommy_vecetti();
+//
+//        Scanner u=new Scanner(System.in);
+//        System.out.println("Here are some commands that you can do to character : ");
+//        System.out.println("R: Run");
+//        System.out.println("F: Fire");
+//        System.out.println("H: Hit");
+//        for (int i=0;i<=2;i++){
+//            if (i==0) {
+//                System.out.print("Do something : ");
+//                String a = u.nextLine();
+//                if (a.equals("R")){
+//                    ok.Run();
+//                }else if (a.equals("H")){
+//                    ok.Hit();
+//                }else if (a.equals("F")){
+//                    ok.Fire();
+//                }
+//            }
+//            if (i>0){
+//                System.out.print("Do something again : ");
+//                String a = u.nextLine();
+//                if (a.equals("R")){
+//                    ok.Run();
+//                }else if (a.equals("H")){
+//                    ok.Hit();
+//                }else if (a.equals("F")){
+//                    ok.Fire();
+//                }
+//            }
+//        }
+//        System.out.println("Ok you are done");
+
+
+
+   }
+}
